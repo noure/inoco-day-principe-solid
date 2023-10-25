0580e7552445e49b932e764530b62e47109494e4
LABIHI Noureddine <labihi.nour@gmail.com>
2023-10-26 01:08:49 +0200

    Inoco-day principes solid

---
+++ 1 - Cours/Principe SOLID/slide.js
@@ -0,0 +1,6 @@
+require(['common/js/order!slide_config', 'common/js/order!common/js/modernizr.custom.45394',
+         'common/js/order!common/js/prettify/prettify', 'common/js/order!common/js/hammer', 
+         'common/js/order!common/js/slide-controller', 'common/js/order!common/js/slide-deck'],
+         function(someModule) {
+
+});
---
+++ 1 - Cours/Principe SOLID/slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Inoco-Day Principes SOLID',
+    subtitle: 'Principes SOLID',
+    eventInfo: {
+      title: 'Inoco-day',
+      date: '26/10/2023'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/common/easymaker_64_50.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Noureddine LABIHI',
+    company: 'Inoco',
+    www: '',
+  }]
+};
+
